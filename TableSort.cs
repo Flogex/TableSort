5b1ea20e90a2fb7a98754cf78845849849042cab
Florian G <gerlinghoff@live.de>
2016-09-22 20:36:51 +0200

    Added TableSort.cs

---
+++ TableSort.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Flogex.SortingAllgorithms.TableSort
+{
+    class TableSort
+    {
+        public static int[] Sort(IList<int> unsortedCollection)
+        {
+            int numberOfColumns = unsortedCollection.Count;
+            int[] sumOfColumn = new int[numberOfColumns];
+            int[] sortedCollection = new int[numberOfColumns];
+
+            for (int x = 0; x < numberOfColumns; x++)
+            {
+                for (int y = x + 1; y < numberOfColumns; y++)
+                {
+                    if (unsortedCollection[x] < unsortedCollection[y])
+                    {
+                        sumOfColumn[y] += 1;
+                    }
+                    else
+                    {
+                        sumOfColumn[x] += 1;
+                    }
+                }
+                sortedCollection[sumOfColumn[x]] = unsortedCollection[x];
+            }
+            return sortedCollection;
+        }
+    }
+}
