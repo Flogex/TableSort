7b6956fe1594a1130ef62935943ffc3daa218573
Florian G <gerlinghoff@live.de>
2016-09-23 14:26:44 +0200

    Using Generics

--- TableSort.cs
+++ TableSort.cs
@@ -1,20 +1,21 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 
 namespace Flogex.SortingAllgorithms.TableSort
 {
-    class TableSort
+    class TableSort<T> where T:IComparable
     {
-        public static int[] Sort(IList<int> unsortedCollection)
+        public static T[] Sort(IList<T> unsortedCollection)
         {
             int numberOfColumns = unsortedCollection.Count;
             int[] sumOfColumn = new int[numberOfColumns];
-            int[] sortedCollection = new int[numberOfColumns];
+            T[] sortedCollection = new T[numberOfColumns];
 
             for (int x = 0; x < numberOfColumns; x++)
             {
                 for (int y = x + 1; y < numberOfColumns; y++)
                 {
-                    if (unsortedCollection[x] < unsortedCollection[y])
+                    if (unsortedCollection[x].CompareTo(unsortedCollection[y]) < 0)
                     {
                         sumOfColumn[y] += 1;
                     }
